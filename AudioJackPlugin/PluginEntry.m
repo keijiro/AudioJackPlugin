d3d0bd12e1a90022806bbfa7964734f54fee3e85
Keijiro Takahashi <keijiro@Lisa.local>
2014-01-03 22:10:57 +0900

    API redesign.

--- AudioJackPlugin/PluginEntry.m
+++ AudioJackPlugin/PluginEntry.m
@@ -6,61 +6,99 @@
 #import "AudioRingBuffer.h"
 #import "SpectrumAnalyzer.h"
 
-static AudioInputHandler *GetAudioInput()
+static BOOL initialized = NO;
+
+#pragma mark Exported Global Functions
+
+void AudioJackInitialize()
 {
-    static bool initialized = false;
-    AudioInputHandler *input = [AudioInputHandler sharedInstance];
     if (!initialized)
     {
-        [input start];
-        initialized = true;
+        [AudioInputHandler.sharedInstance start];
+        initialized = YES;
     }
-    return input;
 }
 
-static SpectrumAnalyzer *GetSpectrumAnalyzer()
+uint32_t AudioJackCountInputChannels()
 {
-    static SpectrumAnalyzer *analyzer = nil;
-    if (!analyzer) analyzer = [[SpectrumAnalyzer alloc] init];
-    return analyzer;
+    return (uint32_t)AudioInputHandler.sharedInstance.ringBuffers.count;
 }
 
-int AudioJackCountChannels()
+#pragma mark Exported Analyzer Functions
+
+SpectrumAnalyzer* AudioJackCreateAnalyzer()
 {
-    return (int)GetAudioInput().ringBuffers.count;
+    return [[SpectrumAnalyzer alloc] init];
 }
 
-float AudioJackGetSampleRate()
+void AudioJackReleaseAnalyzer(SpectrumAnalyzer *analyzer)
 {
-    return GetAudioInput().sampleRate;
+    [analyzer release];
 }
 
-float AudioJackGetChannelLevel(int channel)
+void AudioJackSetDftPointNumber(SpectrumAnalyzer *analyzer, uint32_t number)
 {
-    const float kZeroOffset = 1.5849e-13f;
-    const float kRefLevel = 0.70710678118f; // 1/sqrt(2)
-    
-    AudioInputHandler *input = GetAudioInput();
-    int sampleCount = input.sampleRate / 60; // 60 fps
+    analyzer.pointNumber = number;
+}
+
+void AudioJackSetOctaveBandType(SpectrumAnalyzer *analyzer, uint32_t type)
+{
+    analyzer.octaveBandType = type;
+}
+
+void AudioJackAnalyzeAudioInput(SpectrumAnalyzer *analyzer, uint32_t channel, uint32_t mode)
+{
+    if (mode == 0) {
+        [analyzer processAudioInput:AudioInputHandler.sharedInstance channel:channel];
+    } else if (mode == 1) {
+        [analyzer processAudioInput:AudioInputHandler.sharedInstance channel1:channel channel2:channel + 1];
+    } else {
+        [analyzer processAudioInput:AudioInputHandler.sharedInstance allChannels:YES];
+    }
+}
+
+void AudioJackAnalyzerWaveform(SpectrumAnalyzer *analyzer, const float *waveform1, const float *waveform2, float sampleRate)
+{
+    if (waveform2)
+    {
+        [analyzer processWaveform:waveform1 withAdding:waveform2 samleRate:sampleRate];
+    }
+    else
+    {
+        [analyzer processWaveform:waveform1 samleRate:sampleRate];
+    }
+}
+
+uint32_t AudioJackGetRawSpectrum(SpectrumAnalyzer *analyzer, float *destination)
+{
+    SpectrumDataRef source = analyzer.rawSpectrumData;
+    memcpy(destination, source->data, sizeof(float) * source->length);
+    return (uint32_t)source->length;
+}
+
+uint32_t AudioJackGetOctaveBandSpectrum(SpectrumAnalyzer *analyzer, float *destination)
+{
+    SpectrumDataRef source = analyzer.octaveBandSpectrumData;
+    memcpy(destination, source->data, sizeof(float) * source->length);
+    return (uint32_t)source->length;
+}
+
+#pragma mark Exported RMS Functions
+
+static const float kZeroOffset = 1.5849e-13f;
+static const float kRefLevel = 0.70710678118f; // 1/sqrt(2)
+
+float AudioJackCalculateRmsAudioInput(uint32_t channel, float duration)
+{
+    AudioInputHandler *input = AudioInputHandler.sharedInstance;
+    uint32_t sampleCount = duration * input.sampleRate;
     float rms = [[input.ringBuffers objectAtIndex:channel] calculateRMS:sampleCount];
-    
     return 20.0f * log10f(rms / kRefLevel + kZeroOffset);
 }
 
-void AudioJackGetSpectrum(int channel, int mode, int pointNumber, float *spectrum)
+float AudioJackCalculateRmsWaveform(const float *waveform, uint32_t length)
 {
-    AudioInputHandler *input = GetAudioInput();
-    SpectrumAnalyzer *analyzer = GetSpectrumAnalyzer();
-    
-    analyzer.pointNumber = pointNumber;
-    
-    if (mode == 0) {
-        [analyzer processAudioInput:input channel:channel];
-    } else if (mode == 1) {
-        [analyzer processAudioInput:input channel1:channel channel2:channel + 1];
-    } else {
-        [analyzer processAudioInput:input allChannels:YES];
-    }
-    
-    memcpy(spectrum, analyzer.rawSpectrumData->data, sizeof(float) * pointNumber / 2);
+    float rms;
+    vDSP_rmsqv(waveform, 1, &rms, length);
+    return 20.0f * log10f(rms / kRefLevel + kZeroOffset);
 }
